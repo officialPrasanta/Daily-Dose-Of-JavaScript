3d1a4891d4af6bc217e46e5c2d5a700238365fb3
Prasanta Roy Choudhury <officialprasantaroychoudhury@gmail.com>
2021-06-28 05:05:28 +0530

    Fun Fact 0

---
+++ FunFact_0.js
@@ -0,0 +1,10 @@
+// Useful example of Destructuring
+
+// let a, b = 10, 20;       //Python like structure
+// console.log(a, b);
+// Error
+
+
+let [a, b] = [10, 20];
+console.log(a, b);
+// Works fine
