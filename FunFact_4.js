6bef2d48d2bf1d54a8e6a3dee1bf3f70e47d59d2
Prasanta Roy Choudhury <officialprasantaroychoudhury@gmail.com>
2021-06-27 07:47:01 +0530

    Fun Fact 4

---
+++ FunFact_4.js
@@ -0,0 +1,14 @@
+// Spread Operator
+
+let a = [10, 20, 30, 40];
+let b = ['50', '60'];
+
+// Traditional way
+// d = a.concat(b);
+// console.log(d);
+
+// Cool Way
+let c = [...a, ...b];
+console.log(c);
+
+
