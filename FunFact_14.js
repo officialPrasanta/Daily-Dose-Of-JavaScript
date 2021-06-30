05abe1094dda6cab37bf13e0a83e61129d9126ba
Prasanta Roy Choudhury <officialprasantaroychoudhury@gmail.com>
2021-06-30 06:01:32 +0530

    Fun Fact 14

---
+++ FunFact_14.js
@@ -0,0 +1,11 @@
+const randomXORShift = () => {
+    let seed = new Date();
+    let x0 = seed.getTime();
+
+    x0 ^= x0 << 13;
+    x0 ^= x0 >> 7;
+    x0 ^= x0 << 17;
+    console.log('Random Number(32-bit):', (x0 & 0xFFFF));
+
+}
+randomXORShift();
