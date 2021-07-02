3018ed20b4f8d272f7b4202cbfdfff284a462a47
Prasanta Roy Choudhury <officialprasantaroychoudhury@gmail.com>
2021-07-02 23:40:30 +0530

    Update: regular expression fixed

--- FunFact_12.js
+++ FunFact_12.js
@@ -3,10 +3,11 @@
 let aString =
     "Mr. X's Pin no. is 400 001,\
 Mr. Y's pin number: 600-006.\
-Ms. Z's Pin no. is 400001";
+Ms. Z's Pin no. is 40";
 
 // regex literal to find pin code
-let re = /\d{6}|\d{3}(?:\s\d{3})|\d{3}(?:-\d{3})/gm;
+// let re = /\d{6}|\d{3}(?:\s\d{3})|\d{3}(?:-\d{3})/gm;
+let re = /\b\d{3}[\s, -]?\d{3}\b/gm;
 
 let pinArray = aString.match(re);
 console.log(pinArray);
---
+++ FunFact_16.js
@@ -0,0 +1,27 @@
+let cricketer = {
+    firstName: "Rahul",
+    lastName: "Dravid"
+}
+
+/***Arrow Function***/
+
+// let printBio = (score) => {
+//     console.log("My Favorite Cricketer: ");
+//     console.log(this.firstName + " " + this.lastName);
+//     console.log("He scores over " + score + " run in ODI.");
+// }
+
+// TypeError
+
+
+/***Regular Function***/
+
+function printBio(score) {
+    console.log("My Favorite Cricketer: ");
+    console.log(this.firstName + " " + this.lastName);
+    console.log("He scores over " + score + " run in ODI.");
+}
+
+printBio.call(cricketer, "10000");
+
+// Works Fine
