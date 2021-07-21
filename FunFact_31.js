97635a074c73852b2435843567cd621ece38dfc3
Prasanta Roy Choudhury <officialprasantaroychoudhury@gmail.com>
2021-07-21 17:18:05 +0530

    Fun Fact 31

---
+++ FunFact_31.js
@@ -0,0 +1,14 @@
+
+function randomString(len) {
+    let str = "";
+    let randomNo;
+    for (; str.length < len; str += randomNo) {
+        randomNo = Math.random();
+        randomNo = randomNo.toString(36).substr(2);
+    }
+    return str.substr(0, len);
+}
+
+let rand_str = randomString(10);
+
+console.log(rand_str);
