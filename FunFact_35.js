97e408c2f639e05b30a78bfc357b258b3a800060
Prasanta Roy Choudhury <officialprasantaroychoudhury@gmail.com>
2021-07-26 11:24:33 +0530

    Update: another Cool Way

--- FunFact_35.js
+++ FunFact_35.js
@@ -6,3 +6,19 @@ let taskCompleted = {
 
 let days = Object.keys(taskCompleted);
 days.map((day) => console.log(`${day} => ${taskCompleted[day]}`));
+
+
+// Look Around! Javascript is full of possibilities
+
+
+let taskCompletionArr = Object.entries(taskCompleted);    // to array
+const map = new Map(taskCompletionArr);
+console.log(map);
+
+/*
+Map(3) {
+  'day34' => 'Node and Express tutorial-continued',
+  'day35' => 'Node and Express tutorial-completed',
+  'day36' => 'Music Streaming App with React-Day[1]'
+}
+*/
